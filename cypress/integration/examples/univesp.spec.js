c9ee8c24bf4db0cea2d7fe64189adbdffafdd79b
Leonardo Reina <leonardo@formahomolog.com.br>
2020-05-11 18:22:09 -0300

    fazendo primeira requisicao url

---
+++ cypress/integration/examples/univesp.spec.js
@@ -0,0 +1,7 @@
+describe("Teste básico de login no portal do Canvas", () => {
+    it("Deve logar com usuário válido", () => {
+        
+        cy.visit('http://login.univesp.br')
+    
+    })
+})
