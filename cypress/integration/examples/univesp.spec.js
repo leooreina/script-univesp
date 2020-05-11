9277b926355511775c138597be2b55b740eda007
AutorUnivesp <autor@univesp.br>
2020-05-11 18:38:21 -0300

    add comentarios no teste

--- cypress/integration/examples/univesp.spec.js
+++ cypress/integration/examples/univesp.spec.js
@@ -1,6 +1,7 @@
 describe("Teste básico de login no portal do Canvas", () => {
     it("Deve logar com usuário válido", () => {
         
+        // visitando o site da univesp.
         cy.visit('http://login.univesp.br')
     
     })
