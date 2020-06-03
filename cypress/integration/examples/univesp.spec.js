4557efa9fc2a3b219948c7671d96ab26e0823ff9
GalloGabriel <gabriel.gallo2242@gmail.com>
2020-06-03 18:21:50 -0300

    implementando funcoes

--- cypress/integration/examples/univesp.spec.js
+++ cypress/integration/examples/univesp.spec.js
@@ -10,42 +10,10 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe("Teste básico de login no portal do Canvas", () => {
     it("Deve logar com usuário válido", () => {
-        
-        // visitando o site de login da univesp
-        cy.visit('https://cursos.univesp.br')
-
-        // seleciona o campo de login e digita o username
-        cy.get('#username').type(Cypress.env('USERNAME'))
-
-        // Clica no botão "próximo"
-        cy.get('.step-footer button:first').click()
-
-        // seleciona o campo de login e digita a senha
-        cy.get('#password').type(Cypress.env('PASSWORD'))
-
-        // Clica no botão "enviar"
-        cy.get('.btn-red').contains('Enviar').click()
-
-        cy.request('POST', 'https://cursos.univesp.br/login/saml', 
-        { username: Cypress.env('USERNAME'),
-            password: Cypress.env('PASSWORD')
-        })
-        .then(res => {
-            // cy.visit('https://cursos.univesp.br/simplesaml/module.php/core/loginuserpass.php?AuthState=_106fc02920d697261542702f0116517e576cc9bb24%3Ahttps%3A%2F%2Flogin.univesp.br%2Fsimplesaml%2Fsaml2%2Fidp%2FSSOService.php%3Fspentityid%3Dhttp%253A%252F%252Fcursos1.univesp.br%252Fsaml2%26cookieTime%3D1589588198')
-            
-            cy.wait(5000)
-
-            cy.get('#global_nav_courses_link').click()
-
-            cy.wait(2000)
-
-            cy.get('a').contains('Todos os Cursos').click()
-
-            cy.contains('AAG002').click()
-
-            cy.contains('Poder e Cultura Organizacional').next().click()
-
-            cy.get('span').contains('Plano de Ensino').should('be.visible')
+        cy.login().then(res => {
+            cy.verificaDesbloqueio('AAG002', '.semana-4')
+            cy.verificaDesbloqueio('EIR201', '.semana-4')
+            cy.verificaDesbloqueio('EID002', '.semana-4')
         })
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,42 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('verificaDesbloqueio',
+ (codigo, classe) => {
+    cy.get('#global_nav_courses_link').click()
+
+    cy.wait(5000)
+
+    cy.get('a').contains('Todos os Cursos').click()
+
+    cy.contains(codigo).click()
+
+    cy.get(classe).click()
+
+    cy.get('span').contains('Plano de Ensino').should('be.visible')
+ }) 
+
+ Cypress.Commands.add('login', () => {
+   // visitando o site de login da univesp
+   cy.visit('https://cursos.univesp.br')
+
+   // seleciona o campo de login e digita o username
+   cy.get('#username').type(Cypress.env('USERNAME'))
+
+   // Clica no botão "próximo"
+   cy.get('.step-footer button:first').click()
+
+   // seleciona o campo de login e digita a senha
+   cy.get('#password').type(Cypress.env('PASSWORD'))
+
+   // Clica no botão "enviar"
+   cy.get('.btn-red').contains('Enviar').click()
+
+   cy.request('POST', 'https://cursos.univesp.br/login/saml', 
+   { username: Cypress.env('USERNAME'),
+       password: Cypress.env('PASSWORD')
+   })
+ })
+            
